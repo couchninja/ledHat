efeedb352a961854d1b4cc2f616856a2803b33ce
aronabbo <aron.abbo@gmail.com>
2018-04-01 18:28:01 +0200

    final Get rich or die trying version

--- src/animation/Animation.cpp
+++ src/animation/Animation.cpp
@@ -36,7 +36,7 @@ void Animation::setPixelFromBottomF(uint8_t rIndex, float y, CHSV chsv) {
  * y: vertical index [0, numOfStrips> from bottom to top
  */
 void Animation::setPixelFromTop(uint8_t rIndex, uint8_t y, CHSV chsv) {
-	if(y < numStrips) {
+	if (y < numStrips) {
 		rIndex %= ledsPerStrip;
 		uint8_t rCounterClock = ledsPerStrip - rIndex - 1;
 		leds[y * ledsPerStrip + rCounterClock] = chsv;
@@ -49,20 +49,23 @@ void Animation::setPixelFromTop(uint8_t rIndex, uint8_t y, CHSV chsv) {
  * y: vertical index [0, numOfStrips> from bottom to top
  */
 void Animation::setPixelFromTopF(uint8_t rIndex, float y, CHSV chsv) {
-	float rCounterClock = ledsPerStrip - rIndex - 1;
+	rIndex %= ledsPerStrip;
+	if (y < numStrips) {
+		float rCounterClock = ledsPerStrip - rIndex - 1;
 
-	uint8_t yBott = (uint8_t) y;
-	uint8_t yTop = yBott + 1;
+		uint8_t yBott = (uint8_t) y;
+		uint8_t yTop = yBott + 1;
 
-	float topPart = y - yBott;
-	float bottomPart = 1.0 - topPart;
+		float topPart = y - yBott;
+		float bottomPart = 1.0 - topPart;
 
-	// clamping is needed for safety since we set chsv directly
-	leds[clamp(yBott * ledsPerStrip + rCounterClock)].setHSV(chsv.h, chsv.s,
-			chsv.v * bottomPart);
+		// clamping is needed for safety since we set chsv directly
+		leds[clamp(yBott * ledsPerStrip + rCounterClock)].setHSV(chsv.h, chsv.s,
+				chsv.v * bottomPart);
 
-	leds[clamp(yTop * ledsPerStrip + rCounterClock)].setHSV(chsv.h, chsv.s,
-			chsv.v * topPart);
+		leds[clamp(yTop * ledsPerStrip + rCounterClock)].setHSV(chsv.h, chsv.s,
+				chsv.v * topPart);
+	}
 }
 
 uint8_t Animation::clamp(uint8_t ledCoord) {
