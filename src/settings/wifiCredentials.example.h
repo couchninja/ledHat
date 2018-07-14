9678225a902bb81077535eed39da87208a36ab80
aronabbo <aron.abbo@gmail.com>
2018-07-14 10:15:32 +0200

    add wifi example file

---
+++ src/settings/wifiCredentials.example.h
@@ -0,0 +1,10 @@
+#ifndef SRC_WIFICREDENTIALS_H_
+#define SRC_WIFICREDENTIALS_H_
+
+class WifiCredentials {
+public:
+	static constexpr const char* ssid = "";
+	static constexpr const char* password = "";
+};
+
+#endif /* SRC_WIFICREDENTIALS_H_ */
